ef785b8d6bb9577269d2eb9a2c34a045a2cfef38
Grayson Cunningham <gecun289@gmail.com>
2021-01-14 18:47:20 -0700

    add functionality to create new discounts

--- app/controllers/merchants/discounts_controller.rb
+++ app/controllers/merchants/discounts_controller.rb
@@ -6,4 +6,27 @@ class Merchants::DiscountsController < ApplicationController
   def show
     @discount = Discount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = @merchant.discounts.new(discount_params)
+    if @discount.save
+      flash.notice = ["Discount Successfully Added"]
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash.alert = @discount.errors.full_messages
+      render :new, obj: [@merchant, @discount]
+    end
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:percentage, :threshold)
+  end
 end
