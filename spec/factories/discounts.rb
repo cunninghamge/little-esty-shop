b1694423d8d9db6b28551c68468d6a1cef5b0b55
Grayson Cunningham <gecun289@gmail.com>
2021-01-16 15:32:28 -0700

    add discounts factory

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     association :merchant
     percentage {rand(5..40)}
     threshold {rand(2..20)}
+
+    factory :discount_1 do
+      percentage {20}
+      threshold {10}
+    end
   end
 end
--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :invoice_item, class: InvoiceItem do
     association :item
     association :invoice
+    association :discount
     quantity { Faker::Number.between(from: 1, to: 10) }
     unit_price { Faker::Number.between(from: 1, to: 100) }
     status {["pending", "packaged", "shipped"].sample}
