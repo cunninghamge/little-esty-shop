86c436ded6b94010d06c1dc6fe34c6255d291d2b
Grayson Cunningham <gecun289@gmail.com>
2021-01-16 18:04:41 -0700

    add helper method for displaying discount details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def format_price(price)
     number_to_currency(price / 100.0)
   end
+
+  def discount_details(discount)
+    "#{discount.percentage}% off orders of #{discount.threshold} or more"
+  end
 end
