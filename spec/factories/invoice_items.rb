9f500c2ee5a44e6892f7b490960e96493952b909
Grayson Cunningham <gecun289@gmail.com>
2021-01-16 18:23:17 -0700

    minor changes to factories

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     association :invoice
     association :discount
     quantity { Faker::Number.between(from: 1, to: 10) }
-    unit_price { Faker::Number.between(from: 1, to: 100) }
+    unit_price { Faker::Number.between(from: 50, to: 100000) }
     status {["pending", "packaged", "shipped"].sample}
 
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item, class: Item do
     name { Faker::Commerce.product_name }
     description { Faker::Quote.famous_last_words }
-    unit_price { Faker::Number.between(from: 1, to: 100) }
+    unit_price { Faker::Number.between(from: 50, to: 10000) }
     association :merchant
 
     trait :with_status do
