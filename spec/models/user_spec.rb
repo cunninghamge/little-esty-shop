6a3911a204dffc94db93cd358510c49df2a96a49
Grayson Cunningham <gecun289@gmail.com>
2021-01-17 09:24:46 -0700

    add role enum to user model with tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe User do
     it {should validate_uniqueness_of(:username)}
     it {should validate_presence_of(:password)}
   end
+
+  describe 'roles' do
+    it "can be a default user" do
+      user = create(:user)
+
+      expect(user.role).to eq("default")
+    end
+
+    it "can be a merchant" do
+      user = create(:user, role: 1)
+
+      expect(user.role).to eq("merchant")
+    end
+
+    it "can be an admin" do
+      user = create(:user, role: 2)
+
+      expect(user.role).to eq("admin")
+    end
+  end
 end
