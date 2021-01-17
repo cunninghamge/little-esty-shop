215503ef790b32ab2c7051842efb995724112223
Grayson Cunningham <gecun289@gmail.com>
2021-01-16 22:30:17 -0700

    add tests for nav bar items and navigation permissions by user type

--- spec/features/nav_spec.rb
+++ spec/features/nav_spec.rb
@@ -1,11 +1,172 @@
 require 'rails_helper'
 
-RSpec.describe "Nav bar" do
-  it "has links to basic navigation pages" do
-    visit admin_path
+RSpec.describe "navigation bar" do
+  describe "options by user" do
+    it "visitor" do
+      visit root_path
 
-    expect(page).to have_link("Admin Dashboard", href: admin_path)
-    expect(page).to have_link("Admin Merchants Index", href: admin_merchants_path)
-    expect(page).to have_link("Merchants Index", href: merchants_path)
+      within("#nav") do
+        expect(page).to have_link("Home", href: root_path)
+        expect(page).to have_link("Items", href: items_path)
+        expect(page).to have_link("Log In", href: login_path)
+        expect(page).to have_link("Register", href: register_path)
+        expect(page).to have_content("My Cart (0)")
+      end
+    end
+
+    it "default user" do
+      user = create(:user, role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+
+      within("#nav") do
+        expect(page).to have_link("Home", href: root_path)
+        expect(page).to have_link("Items", href: items_path)
+        expect(page).to have_content("Logged in as #{user.username}")
+        expect(page).to have_link("Log Out", href: login_path)
+        expect(page).to have_content("My Cart (0)")
+        expect(page).not_to have_link("Log In")
+        expect(page).not_to have_link("Register")
+      end
+    end
+
+    it "merchant" do
+      user = create(:user, role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_merchant_path
+
+      within("#nav") do
+        expect(page).to have_link("Home", href: root_path)
+        expect(page).to have_content("Logged in as #{user.username}")
+        expect(page).to have_link("Log Out", href: login_path)
+        expect(page).to have_link("Dashboard", href: dashboard_merchant_path)
+        expect(page).not_to have_link("Items")
+        expect(page).to have_content("My Cart (0)")
+        expect(page).not_to have_link("Log In")
+        expect(page).not_to have_link("Register")
+      end
+    end
+
+    it "as an admin" do
+      user = create(:user, role: 2)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_path
+
+      within("#nav") do
+        expect(page).to have_link("Home", href: root_path)
+        expect(page).to have_content("Logged in as #{user.username}")
+        expect(page).to have_link("Log Out", href: login_path)
+        expect(page).to have_link("Dashboard", href: admin_path)
+        expect(page).not_to have_link("Items")
+        expect(page).to have_content("My Cart (0)")
+        expect(page).not_to have_link("Log In")
+        expect(page).not_to have_link("Register")
+      end
+    end
+  end
+
+  describe "restrictions" do
+    describe "does not allow a visitor to access merchant or admin pages" do
+      it {visit dashboard_merchant_path}
+      it {visit merchant_invoices_path}
+      it {visit merchant_items_path}
+      it {visit merchant_discounts_path}
+      it {visit admin_path}
+      it {visit admin_invoices_path}
+      it {visit admin_merchants_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(404)
+      end
+    end
+
+    describe "does not allow a default user to access merchant or admin pages" do
+      before(:each) do
+        user = create(:user, role: 0)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+
+      it {visit dashboard_merchant_path}
+      it {visit merchant_invoices_path}
+      it {visit merchant_items_path}
+      it {visit merchant_discounts_path}
+      it {visit admin_path}
+      it {visit admin_invoices_path}
+      it {visit admin_merchants_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(404)
+      end
+    end
+
+    describe "does not allow a default user to access merchant or admin pages" do
+      before(:each) do
+        user = create(:user, role: 0)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+      it {visit dashboard_merchant_path}
+      it {visit merchant_invoices_path}
+      it {visit merchant_items_path}
+      it {visit merchant_discounts_path}
+      it {visit admin_path}
+      it {visit admin_invoices_path}
+      it {visit admin_merchants_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(404)
+      end
+    end
+
+    describe "does not allow a merchant to access admin pages" do
+      before(:each) do
+        user = create(:user, role: 1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+
+      it {visit admin_path}
+      it {visit admin_invoices_path}
+      it {visit admin_merchants_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(404)
+      end
+    end
+
+    describe "allows a merchant to access merchant pages" do
+      before(:each) do
+        user = create(:user, role: 1)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+
+      it {visit dashboard_merchant_path}
+      it {visit merchant_invoices_path}
+      it {visit merchant_items_path}
+      it {visit merchant_discounts_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(200)
+      end
+    end
+
+    describe "allows an admin to access admin pages" do
+      before(:each) do
+        user = create(:user, role: 2)
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      end
+
+      it {visit admin_path}
+      it {visit admin_invoices_path}
+      it {visit admin_merchants_path}
+
+      after(:each) do
+        expect(page.status_code).to eq(200)
+      end
+    end
   end
 end
