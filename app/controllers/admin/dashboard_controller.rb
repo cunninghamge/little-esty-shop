d7209358cd23b0b1b61906c6d38d8e851f49c825
Grayson Cunningham <gecun289@gmail.com>
2021-01-17 11:56:04 -0700

    add admin base controller and admin authorization methods

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class Admin::DashboardController < ApplicationController
+class Admin::DashboardController < Admin::BaseController
   def index
     @customers = Customer.top_customers(5)
     @incomplete_invoices = Invoice.incomplete_invoices
