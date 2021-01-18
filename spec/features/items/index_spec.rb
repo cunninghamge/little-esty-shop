addd06486392ec7d9914d0e18738739bde7f1c70
Grayson Cunningham <gecun289@gmail.com>
2021-01-17 17:21:49 -0700

    add functionality to view all enabled items on items index

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Items Index" do
+  include ApplicationHelper
+  include ActionView::Helpers::NumberHelper
+
   it "lists all of the items" do
     items = create_list(:item, 3)
     visit items_path
@@ -9,10 +12,20 @@ RSpec.describe "Items Index" do
       within("#item-#{item.id}") do
         expect(page).to have_content(item.name)
         expect(page).to have_content(item.description)
-        expect(page).to have_content(item.price)
+        expect(page).to have_content(format_price(item.unit_price))
         expect(page).to have_select(:quantity)
         expect(page).to have_button("Add to Cart")
       end
     end
+  end
+
+  it "only lists enabled items" do
+    enabled_item = create(:item, enabled: true)
+    disabled_item = create(:item, enabled: false)
+
+    visit items_path
+
+    expect(page).to have_css("#item-#{enabled_item.id}")
+    expect(page).not_to have_css("#item-#{disabled_item.id}")
   end
 end
