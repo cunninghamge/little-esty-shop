fd9bf5887d25cd2c62046e1b93b572ce8390792e
Grayson Cunningham <gecun289@gmail.com>
2021-01-17 21:36:26 -0700

    add invoices controller with methods to create invoice

--- spec/factories/invoice_items.rb
+++ spec/factories/invoice_items.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :invoice_item, class: InvoiceItem do
     association :item
     association :invoice
-    association :discount
     quantity { Faker::Number.between(from: 1, to: 10) }
     unit_price { Faker::Number.between(from: 50, to: 100000) }
     status {["pending", "packaged", "shipped"].sample}
--- spec/features/cart/check_out_spec.rb
+++ spec/features/cart/check_out_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "checking out" do
-  it "users see a button to check out" do
-    cart = Cart.new({'1'=> 2, '2'=>3})
-    allow_any_instance_of(ApplicationController).to receive(:cart).and_return(cart)
-    visit items_path
+  describe "checkout button" do
+    it "users see a button to check out" do
+      cart = Cart.new({'1'=> 2, '2'=>3})
+      allow_any_instance_of(ApplicationController).to receive(:cart).and_return(cart)
+      visit items_path
 
-    expect(page).to have_button("Check Out")
+      expect(page).to have_button("Check Out")
+    end
+
+    it "does not allow the user to check out if the cart is empty" do
+      visit items_path
+
+      expect(page).not_to have_button("Check Out")
+    end
   end
 
-  it "does not allow the user to check out if the cart is empty" do
+  it "creates an invoice and invoice_items" do
+    item = create(:item)
+    cart = Cart.new({"#{item.id}"=> 1})
+    customer = create(:customer)
+    user = create(:user, role: 0, customer: customer)
+    allow_any_instance_of(ApplicationController).to receive(:cart).and_return(cart)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     visit items_path
 
-    expect(page).not_to have_button("Check Out")
+    click_button("Check Out")
+
+    expect(page).to have_content("Order Submitted")
+    expect(customer.invoices.count).to eq(1)
+    expect(customer.invoices[0].invoice_items.count).to eq(1)
   end
 end
