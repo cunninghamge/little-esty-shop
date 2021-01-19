cd9351781f38367972f2b7146b88d04a8a2b067b
Grayson Cunningham <gecun289@gmail.com>
2021-01-19 14:59:29 -0700

    add additional orders for merchant users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Transaction.destroy_all
 InvoiceItem.destroy_all
@@ -15,20 +8,16 @@ Customer.destroy_all
 Merchant.destroy_all
 
 created_at = 1.year.ago
-
+merchant_users = []
 User.create(username: "admin", password: "admin", role: 2)
-FactoryBot.create(:merchant, id: 1, created_at: created_at)
+merchant_users << FactoryBot.create(:merchant, id: 1, created_at: created_at)
 User.create(username: "merchant_1", password: "merchant", role: 1, merchant_id: 1)
-FactoryBot.create(:merchant, id: 2, created_at: created_at)
+merchant_users << FactoryBot.create(:merchant, id: 2, created_at: created_at)
 User.create(username: "merchant_2", password: "merchant", role: 1, merchant_id: 2)
-FactoryBot.create(:merchant, id: 3, created_at: created_at)
+merchant_users << FactoryBot.create(:merchant, id: 3, created_at: created_at)
 User.create(username: "merchant_3", password: "merchant", role: 1, merchant_id: 3)
 FactoryBot.create(:customer, id: 1, created_at: created_at)
-User.create(username: "customer_1", password: "customer", role: 0, customer_id: 1)
-FactoryBot.create(:customer, id: 2, created_at: created_at)
-User.create(username: "customer_2", password: "customer", role: 0, customer_id: 2)
-FactoryBot.create(:customer, id: 3, created_at: created_at)
-User.create(username: "customer_3", password: "customer", role: 0, customer_id: 3)
+User.create(username: "customer", password: "customer", role: 0, customer_id: 1)
 
 FactoryBot.create_list(:item, rand(2..30), merchant_id: 1, created_at: created_at)
 FactoryBot.create_list(:discount, rand(0..3), merchant_id: 1, created_at: created_at)
@@ -43,18 +32,36 @@ FactoryBot.create_list(:discount,rand(0..3), merchant_id: 1, created_at: created
   FactoryBot.create_list(:discount, rand(0..2), merchant: merchant)
 end
 
-FactoryBot.create_list(:customer, 197)
+FactoryBot.create_list(:customer, 199)
 
 #in progress orders with successful transaction, no items shipped
+merchant_users.each do |merchant|
+  10.times do
+    invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..10).days.ago)
+    2.times do
+      FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at)
+    end
+    FactoryBot.create(:transaction, invoice: invoice, result: 0)
+  end
+end
 100.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..10).days.ago)
   rand(1..6).times do
-    invoice_item = FactoryBot.create(:invoice_item, invoice: invoice, item: Item.order('random()').first, created_at: invoice.created_at)
+    FactoryBot.create(:invoice_item, invoice: invoice, item: Item.order('random()').first, created_at: invoice.created_at)
   end
   FactoryBot.create(:transaction, invoice: invoice, result: 0)
 end
 
 #in progress orders with successful transaction, some items shipped
+merchant_users.each do |merchant|
+  15.times do
+    invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..10).days.ago)
+    2.times do
+      FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at, status: rand(0..2))
+    end
+    FactoryBot.create(:transaction, invoice: invoice, result: 0)
+  end
+end
 150.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..10).days.ago)
   rand(1..6).times do
@@ -64,6 +71,14 @@ end
 end
 
 #in progress orders with multiple transactions, no items shipped
+merchant_users.each do |merchant|
+  invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
+  2.times do
+    FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at)
+  end
+  FactoryBot.create(:transaction, invoice: invoice, result: 1)
+  FactoryBot.create(:transaction, invoice: invoice, result: 0)
+end
 10.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
   rand(1..6).times do
@@ -74,6 +89,14 @@ end
 end
 
 #in progress orders with multiple transactions, some items shipped
+merchant_users.each do |merchant|
+  invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
+  2.times do
+    FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at, status: rand(0..2))
+  end
+  FactoryBot.create(:transaction, invoice: invoice, result: 1)
+  FactoryBot.create(:transaction, invoice: invoice, result: 0)
+end
 10.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
   rand(1..6).times do
@@ -84,6 +107,13 @@ end
 end
 
 #in progress orders with failed transaction, no items shipped
+merchant_users.each do |merchant|
+  invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
+  2.times do
+    FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at)
+  end
+  FactoryBot.create(:transaction, invoice: invoice, result: 1)
+end
 10.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(1..30).days.ago)
   rand(1..6).times do
@@ -93,6 +123,15 @@ end
 end
 
 #completed orders
+merchant_users.each do |merchant|
+  20.times do
+    invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 1)
+    2.times do
+      FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at, status: 2)
+    end
+    FactoryBot.create(:transaction, invoice: invoice, result: 0)
+  end
+end
 1000.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 1)
   rand(1..6).times do
@@ -102,6 +141,16 @@ end
 end
 
 #completed orders w/ multiple transactions
+merchant_users.each do |merchant|
+  3.times do
+    invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 1)
+    2.times do
+      FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at, status: 2)
+    end
+    FactoryBot.create(:transaction, invoice: invoice, result: 1)
+    FactoryBot.create(:transaction, invoice: invoice, result: 0)
+  end
+end
 30.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 1)
   rand(1..6).times do
@@ -112,10 +161,19 @@ end
 end
 
 #cancelled orders
+merchant_users.each do |merchant|
+  3.times do
+    invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 2)
+    2.times do
+      FactoryBot.create(:invoice_item, invoice: invoice, item: merchant.items.order('random()').first, created_at: invoice.created_at)
+    end
+    FactoryBot.create(:transaction, invoice: invoice, result: 1)
+  end
+end
 30.times do
   invoice = FactoryBot.create(:invoice, customer: Customer.order('random()').first, created_at: rand(2..364).days.ago, status: 2)
   rand(1..6).times do
-    FactoryBot.create(:invoice_item, invoice: invoice, item: Item.order('random()').first, created_at: invoice.created_at, status: rand(0..2))
+    FactoryBot.create(:invoice_item, invoice: invoice, item: Item.order('random()').first, created_at: invoice.created_at)
   end
-  FactoryBot.create(:transaction, invoice: invoice, result: 0)
+  FactoryBot.create(:transaction, invoice: invoice, result: 1)
 end
