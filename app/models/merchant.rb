828c183ef6b151709b35ff54d819ab5a49f523ff
Grayson Cunningham <gecun289@gmail.com>
2021-01-19 23:15:15 -0700

    refactor models to remove relationship between merchants and invoices

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,23 +1,25 @@
 class Merchant < ApplicationRecord
   has_many :items
-  has_many :invoices
+  has_many :invoices, through: :items
   has_many :discounts
-  has_many :customers,-> {distinct}, through: :invoices
 
   validates :name, presence: true
 
-  delegate :top_customers, to: :customers
   delegate :items_to_ship, to: :items
   delegate :total_revenue, to: :invoices
   delegate :top_sales_day, to: :invoices
 
   delegate :discounts_index, to: :discounts
 
+  def top_customers
+    Customer.joins(invoices: [:transactions, :items]).select("customers.*, count(*) as count").where(transactions: {result: 0}, items: {merchant_id: id}).group(:id).order("count"=> :desc).limit(5)
+  end
+
   def self.top_merchants(number = 5)
-    joins(invoices: [:transactions, :invoice_items])
+    joins(invoices: :transactions)
     .where("result = 0")
     .group(:id)
-    .select('merchants.*, sum(quantity * unit_price) as total_revenue')
+    .select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as total_revenue')
     .order("total_revenue" => :desc)
     .limit(number)
   end
